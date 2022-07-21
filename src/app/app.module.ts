7123c32cdff6294b17905d32379032afc0bbbe51
viettu2k <doanviettu2k@gmail.com>
2022-07-21 13:31:51 +0700

    styling login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   // { path: 'search/:searchItem', component: HomeComponent },
   // { path: 'tag/:tag', component: HomeComponent },
   // { path: 'food/:id', component: FoodPageComponent },
